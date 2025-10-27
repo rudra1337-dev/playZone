7e26fcfee2b2588b13ec3c90117f910521533206
rudra1337-dev <rudra1337.dev@gmail.com>
2025-10-27 21:04:14 +0530

    updated

--- java/DSA/c.java
+++ java/DSA/c.java
@@ -1,4 +1,24 @@
 
 public class c {
+  
+  public static void binary(int arr[], int k){
+    int st=0, end=arr.length-1, mid= (st+end)/2;
     
+    for(int i = 0; i<=arr.length-1; i++){
+      if(arr[mid] == k){
+      System.out.println(k+ " is found");
+      return;
+      }else if(arr[mid] > k){
+        end = mid-1;
+      }else if(arr[mid] < k){
+        st = mid+1;
+      }
+    }
+    System.out.println(k+" not found");
+    
+  }
+    public static void main(String args[]){
+      int[] arr = {1,4,6,8};
+      binary(arr, 9);
+    }
 }
