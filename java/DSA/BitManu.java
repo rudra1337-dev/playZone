86c11e481584966d0a110e6e9bd23bdf649cda9d
Rudra <rudra1337.dev@gmail.com>
2025-12-01 09:01:24 +0000

    Bitmanu is updated

--- java/DSA/BitManu.java
+++ java/DSA/BitManu.java
@@ -1,7 +1,7 @@
 
 public class BitManu{
 
-
+    //CHECK N IS EVEN OR ODD
     public static void evenOrOdd(int n){
         int bitMask = 1;
 
@@ -12,12 +12,36 @@ public class BitManu{
         }
     }
 
+    //GET ITH BIT
+    public static void getIthBit(int n, int i){
+        int bitMask = 1<<i;
+
+        if((n&bitMask)==0){
+            System.out.println(i+"th bit is 0");
+        }else{
+            System.out.println(i+"th bit is 1");
+        }
+    }
+
+    //SET ITH BIT TO 1
+    public static void setIthBit(int n, int i){
+        int bitMask = 1<<i;
+
+        n = n|bitMask;
+        System.out.println(n);
+    }
+
     public static void main(String[] args){
         System.out.println("WELCOMETO BITMANUPULATIONS");
 
         evenOrOdd(3);
         evenOrOdd(4);
 
+        getIthBit(3,2);
+        getIthBit(4, 2);
+
+        setIthBit(3,2);
+        setIthBit(4,3);
 
     }
 }
