4133a7e26707f45552abc0d5cc35db860d5acdd1
rudra1337-dev <rudra1337.dev@gmail.com>
2025-12-04 10:08:13 +0530

    Recursion updated

---
+++ java/DSA/Recursion.java
@@ -0,0 +1,118 @@
+
+
+
+public class Recursion{
+  
+  public static void printNto1(int n){
+    System.out.println("DEBUG n = " + n);   // 👈 ADD THIS
+
+    if(n == 1){
+        System.out.print(n + ",");
+        return;
+    }
+
+    System.out.print(n + ",");
+    printNto1(n - 1);
+}
+
+public static void print1toN(int n){
+  if(n == 0){
+    return;
+  }
+  
+  print1toN(n-1);
+  System.out.println(n+",");
+}
+
+public static int factorial(int n){
+  if(n==1 || n==0) return 1;
+  
+  return n*factorial(n-1);
+}
+
+public static int printSumOfNnatu(int n){
+  if(n == 1) return 1;
+  
+  return n+printSumOfNnatu(n-1);
+}
+
+
+public static int fibonachhie(int n){
+  if(n==1) return 1;
+  if(n==0) return 0;
+  
+  return fibonachhie(n-1)+fibonachhie(n-2);
+}
+
+  public static boolean isSorted(int[] arr, int st, int end){
+    if(st >= (end-1)) return true;
+    
+    if(arr[st] > arr[st+1]) return false;
+    
+    return isSorted(arr, st+1, end);
+  }
+  
+  
+  public static int firstOccurance(int n, int[] arr, int st){
+    if(st == arr.length-1) return -1;
+    
+    if(arr[st] == n) return st;
+    return firstOccurance(n, arr, st+1);
+  }
+  
+  
+  public static int lastOccurance(int n, int[] arr, int end){
+    if(end == 0) return -1;
+    
+    if(arr[end] == n) return end;
+    return lastOccurance(n, arr, end-1);
+  }
+  
+  
+  public static int Pow(int n, int p){
+    if(p==1) return n;
+    
+    return n*Pow(n, p-1);
+  }// time complexity = O(n)
+  
+  
+  public static int OptimizedPow(int n, int p){
+    if(p == 0) return 1;
+    
+    int half = OptimizedPow( n, p/2);
+    int power = half*half;
+    
+    //Odd case
+    if(p%2  != 0) power *= n;
+    
+    return power;
+  }
+  
+  
+  public static void main(String args[]){
+    System.out.println("WELCOME TO RECURSION");
+    
+    printNto1(10);
+    print1toN(10);
+    System.out.println("Factorial = "+factorial(10));
+    System.out.println("Sum of n natural no ="+printSumOfNnatu(49));
+    System.out.println("Fibonachhie ="+fibonachhie(10));
+    
+    int arr1[] = {1,3,4,6,7,7,7,8};
+    int arr2[] = {1,3,4,6,7,9,7,8};
+    System.out.println("Array is Sorted = "+isSorted(arr1, 0, arr1.length-1));
+    System.out.println("Array is Sorted = "+isSorted(arr2, 0, arr2.length-1));
+    
+    int arr3[] = {1,4,3,5,8,8,5};
+    System.out.println("First occurance of 5 = "+firstOccurance(5, arr3, 0));
+    System.out.println("Last occurance of 8 = "+lastOccurance(8, arr3,
+    arr3.length-1));
+    
+    System.out.println("5 to the power of 4 ="+Pow(5,4));
+    System.out.println("5 to the power of 4 ="+OptimizedPow(5,4));
+    
+    
+    
+  }
+  
+}
