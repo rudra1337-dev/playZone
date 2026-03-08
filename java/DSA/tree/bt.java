19394f204c6d36dcb6d07a1d60ddd0349ee12313
Rudra <rudra1337.dev@gmail.com>
2026-03-08 12:43:23 +0000

    bt updated

--- java/DSA/tree/bt.java
+++ java/DSA/tree/bt.java
@@ -1,4 +1,6 @@
 
+import java.util.*;
+
 class TreeNode {
     int data;
     TreeNode left;;
@@ -28,60 +30,106 @@ public class bt {
 
         return node;
     }
-    
-    public static void preorderTraversal(TreeNode root){
-      
-      if(root == null){
-        System.out.print(-1+", ");
+
+    public static void preorderTraversal(TreeNode root) {
+
+        if (root == null) {
+            System.out.print(-1 + ", ");
+            return;
+        }
+
+        System.out.print(root.data + ", ");
+
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+
         return;
-      }
-      
-      System.out.print(root.data+", ");
-      
-      preorderTraversal(root.left);
-      preorderTraversal(root.right);
-      
-      return;
     }
-    
-    
-    public static void inorderTraversal(TreeNode root){
-      if(root == null){
+
+    public static void inorderTraversal(TreeNode root) {
+        if (root == null) {
+            System.out.print(-1 + ", ");
+            return;
+        }
+
+        inorderTraversal(root.left);
+        System.out.print(root.data + ", ");
+        inorderTraversal(root.right);
+
         return;
-      }
-      
-      inorderTraversal(root.left);
-      System.out.print(root.data+", ");
-      inorderTraversal(root.right);
-      
-      return;
     }
-    public static void postorderTraversal(TreeNode root){
-      if(root == null){
+
+    public static void postorderTraversal(TreeNode root) {
+        if (root == null) {
+            System.out.print(-1 + ", ");
+            return;
+        }
+
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        System.out.print(root.data + ", ");
+
         return;
-      }
-      
-      postorderTraversal(root.left);
-      postorderTraversal(root.right);
-      System.out.print(root.data+", ");
-      
-      return;
     }
-    
+
+    public static void leavelorderTraversal(TreeNode root) {
+
+        Deque<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        que.add(null);
+
+        while (!que.isEmpty()) {
+
+            TreeNode node = que.remove();
+            if (node == null) {
+                System.out.println();
+
+                if (que.isEmpty())
+                    break;
+
+                que.add(null);
+            } else {
+
+                System.out.print(node.data + ", ");
+
+                if (node.left != null)
+                    que.add(node.left);
+                if (node.right != null)
+                    que.add(node.right);
+            }
+
+        }
+    }
+
+    // Inorder build
+    public static TreeNode buildTreeLevelorder(int[] nums) {
+
+        if()
+
+        TreeNode node = new TreeNode();
+
+        node.left = buildTreeInOrder(nums);
+    }
 
     public static void main(String args[]) {
         System.out.println("Welcome to Tree");
 
         int[] nums = { 1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1 };
+        int[] inorder = { -1, 2, -1, 1, -1, 4, -1, 3, -1, 5, -1, };
+        int[] postorder = { -1, -1, 2, -1, -1, 4, -1, -1, 5, 3, 1, };
+        int[] levelorder = {1, 2, 3, -1, -1, 4, }
 
-        TreeNode root = buildTree(nums);
+        // TreeNode root = buildTree(nums);
+        TreeNode root = buildTreeInOrder(nums);
 
         System.out.println(root.data + " " + root.left.data + " " + root.right.data);
-        
+
         preorderTraversal(root);
         System.out.println();
         inorderTraversal(root);
         System.out.println();
         postorderTraversal(root);
+        System.out.println();
+        leavelorderTraversal(root);
     }
 }
